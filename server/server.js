55d018e1554a9af1b3b1265d9c338428b2d9fa0e
830503 <lixiaoqi19830503@gmail.com>
2021-12-02 14:07:48 +0100

    connected client to the server

--- server/server.js
+++ server/server.js
@@ -4,9 +4,15 @@ const app = express();
 const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 const server = http.createServer(app);
+const io = require('socket.io')(server);
+
 
 
 
 server.listen(8080,() => {
-    console.log("server running on"+8080);
+    console.log("server running on "+8080);
+});
+
+io.on('connection', (socket) => {
+    console.log('someone connected');
 });
